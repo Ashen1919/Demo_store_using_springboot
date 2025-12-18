139837de834d73d8ceb59c007c531948d3cdd1e8
Ashen1919 <2303S0834@saegis.ac.lk>
2025-12-18 20:35:02 +0530

    create new application

---
+++ src/test/java/com/my_project/demo_store/DemoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_project.demo_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
