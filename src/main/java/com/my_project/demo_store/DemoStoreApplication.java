b3ab91a4334016b21d8008d4239137230cd32e54
Ashen1919 <2303S0834@saegis.ac.lk>
2025-12-21 21:57:21 +0530

    create database tables & create two entites

--- src/main/java/com/my_project/demo_store/DemoStoreApplication.java
+++ src/main/java/com/my_project/demo_store/DemoStoreApplication.java
@@ -1,5 +1,6 @@
 package com.my_project.demo_store;
 
+import com.my_project.demo_store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoStoreApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoStoreApplication.class, args);
+        var user = User.builder()
+                .name("John")
+                .password("password")
+                .email("john@example.com")
+                .build();
     }
 
 }
